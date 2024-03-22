bb9f9644c012c00fbd4e0ba2df3291f71390d798
Gasimov Markaz <142661247+mrkzqsmv@users.noreply.github.com>
2024-03-22 16:54:24 +0400

    added check int con;

--- lib/checkInternetConnection/checkInternetConnection.dart
+++ lib/checkInternetConnection/checkInternetConnection.dart
@@ -1,5 +1,19 @@
-class CheckInternetConnection {
-  // Future<bool> checkIntCon() {
-    
-  // }
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class InternetConnectionCheckService with ChangeNotifier {
+  static Future<bool> isInternetAvailable() async {
+    try {
+      final response =
+          await HttpClient().getUrl(Uri.parse("http://google.com/"));
+      final httpResponse = await response.close();
+      if (httpResponse.statusCode == HttpStatus.ok) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
 }
